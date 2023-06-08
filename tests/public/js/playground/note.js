faf89b3da1e0c8a69658df073006acd3c15adbf6
MrBabayser <97740122+MrBabayser@users.noreply.github.com>
2023-06-08 23:32:59 +0200

    Delete tests/public/js directory

--- tests/public/js/playground/note.js
+++
@@ -1,9 +0,0 @@
-function Note (message, avatar, id, pseudo, date) {
-    this.message = message;
-    this.avatar = avatar;
-    this.id = id;
-    this.pseudo = pseudo;
-    this.date = date;
-}
-
-module.exports = Note;
--- tests/public/js/playground/player.js
+++
@@ -1,9 +0,0 @@
-function Player (id, pseudo, avatar, roomId, start) {
-    this.id = id;
-    this.pseudo = pseudo;
-    this.avatar = avatar;
-    this.roomId = roomId;
-    this.start = start;
-}
-
-module.exports = Player;
